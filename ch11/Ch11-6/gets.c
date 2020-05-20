a3ab2680b70deb80f29c4baebc0a0088e4848fa4
silverjin01 <51067408+silverjin01@users.noreply.github.com>
2020-05-20 23:17:55 +0900

    Add files via upload

---
+++ ch11/Ch11-6/gets.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char line[101];
+
+	printf("입력을 종료하려면 새로운 행에서 (crrl + z)를 누르십시오. \n");
+	while (gets(line))
+		puts(line);
+	printf("\n");
+
+	while (gets_s(line, 101))
+		puts(line);
+	printf("\n");
+
+	return 0;
+}
